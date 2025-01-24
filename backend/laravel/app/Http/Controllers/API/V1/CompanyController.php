6ac70ad22ae498a28edd20e1758bd0a3f2e5a39b
vishaalkaran <35380300+vishaalkaran@users.noreply.github.com>
2025-01-24 11:43:46 -0700

    custom route and custom skeleton files for API

---
+++ backend/laravel/app/Http/Controllers/API/V1/CompanyController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Return all companies
+        return Company::paginate();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Company $company)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company)
+    {
+        //
+    }
+}
---
+++ backend/laravel/app/Http/Controllers/API/V1/EmployeeCertificateController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Models\EmployeeCertificate;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EmployeeCertificateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EmployeeCertificate $employeeCertificate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EmployeeCertificate $employeeCertificate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EmployeeCertificate $employeeCertificate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EmployeeCertificate $employeeCertificate)
+    {
+        //
+    }
+}
